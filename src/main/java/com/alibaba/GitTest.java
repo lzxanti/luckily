5046bb790ea5d07675f57bf7251d4adadbe51de9
lzxanti <1972401425@qq.com>
2019-09-15 23:18:34 +0800

    IDEA整合GIT测试

---
+++ src/main/java/com/alibaba/GitTest.java
@@ -0,0 +1,15 @@
+package com.alibaba;
+
+/**
+ * 作用：
+ * <p>@author anti</p>
+ * <p>QQ：1972401425</p>
+ * <p>Date：2019/9/15 23:17 </p>
+ * <p>Version：1.0</p>
+ * <p>Description：</p>
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+    }
+}
